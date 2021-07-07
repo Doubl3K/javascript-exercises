e67862e27e2d7d904b227a428f7630fc8710e31c
Kevin Kinner <kevin-kinner@web.de>
2021-07-07 16:14:16 +0200

    Finished Did copy paste the part about how to find year of Death if not in Object

--- findTheOldest/findTheOldest.js
+++ findTheOldest/findTheOldest.js
@@ -1,15 +1,17 @@
-const findTheOldest = function(people) {
-  const age = people.sort(function(b,a){
-    if (a.yearOfDeath - a.yearOfBirth > b.yearOfDeath - b.yearOfBirth) {
-      return -1;
-    }
-    else {
-      return 1;
-    }
-  });
-  console.log(age);
-  let last = age.pop();
-  return last;
-}
+
+const findTheOldest = function(array) {
+  return array.reduce((oldest, currentPerson) => {
+    const oldestAge = getAge(oldest.yearOfBirth, oldest.yearOfDeath)
+    const currentAge = getAge(currentPerson.yearOfBirth, currentPerson.yearOfDeath)
+    return oldestAge < currentAge ? currentPerson : oldest
+  })
+};
+
+const getAge = function(birth, death) {
+  if (!death) {
+    death = new Date().getFullYear();
+  }
+  return death - birth;
+};
 
 module.exports = findTheOldest;
